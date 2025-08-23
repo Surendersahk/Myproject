a0e2f094b866cdb4d80867819c6c002e88c9da5c
Dhinakar28 <dhinakarpalani3@gmail.com>
2025-08-23 21:32:22 +0530

    dhin5

---
+++ myApp/register.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBXlxzt77Aj4zR-4gAq3K2lf9sGiaEP5t8",
+    authDomain: "helpnet-ed918.firebaseapp.com",
+    projectId: "helpnet-ed918",
+    storageBucket: "helpnet-ed918.firebasestorage.app",
+    messagingSenderId: "720997916359",
+    appId: "1:720997916359:web:4fe91c39292df781ff68e2"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
